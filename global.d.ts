cd0fe0b52f622c10c94b7d265dba9ac7d8da2433
Julius Niiniranta <juliusn@users.noreply.github.com>
2023-10-10 13:22:39 +0300

    update database types

--- global.d.ts
+++ global.d.ts
@@ -2,7 +2,7 @@ import type { Database as DB } from '@/lib/database.types';
 
 declare global {
   type Database = DB;
-  type Todo = DB['public']['Tables']['todos']['Row'];
+  type Profile = Database['public']['Tables']['profiles']['Row'];
 }
 
 // Use type safe message keys with `next-intl`
--- lib/database.types.ts
+++ lib/database.types.ts
@@ -11,89 +11,28 @@ export interface Database {
     Tables: {
       profiles: {
         Row: {
-          avatar_url: string | null
-          full_name: string | null
+          account_type: Database["public"]["Enums"]["account_type_enum"]
+          approval_status: Database["public"]["Enums"]["approval_status_enum"]
+          approval_status_set_by: string | null
           id: string
           updated_at: string | null
-          username: string | null
-          website: string | null
+          user_name: string
         }
         Insert: {
-          avatar_url?: string | null
-          full_name?: string | null
+          account_type?: Database["public"]["Enums"]["account_type_enum"]
+          approval_status?: Database["public"]["Enums"]["approval_status_enum"]
+          approval_status_set_by?: string | null
           id: string
           updated_at?: string | null
-          username?: string | null
-          website?: string | null
+          user_name: string
         }
         Update: {
-          avatar_url?: string | null
-          full_name?: string | null
+          account_type?: Database["public"]["Enums"]["account_type_enum"]
+          approval_status?: Database["public"]["Enums"]["approval_status_enum"]
+          approval_status_set_by?: string | null
           id?: string
           updated_at?: string | null
-          username?: string | null
-          website?: string | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "profiles_id_fkey"
-            columns: ["id"]
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      install_available_extensions_and_test: {
-        Args: Record<PropertyKey, never>
-        Returns: boolean
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
-Need to install the following packages:
-supabase@1.100.1
-Ok to proceed? (y) Need to install the following packages:
-supabase@1.100.1
-Ok to proceed? (y) export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
-
-export interface Database {
-  public: {
-    Tables: {
-      profiles: {
-        Row: {
-          account_type: Database["public"]["Enums"]["account_type_enum"] | null
-          id: string
-          updated_at: string | null
-          user_name: string | null
-        }
-        Insert: {
-          account_type?: Database["public"]["Enums"]["account_type_enum"] | null
-          id: string
-          updated_at?: string | null
-          user_name?: string | null
-        }
-        Update: {
-          account_type?: Database["public"]["Enums"]["account_type_enum"] | null
-          id?: string
-          updated_at?: string | null
-          user_name?: string | null
+          user_name?: string
         }
         Relationships: [
           {
@@ -116,6 +55,7 @@ export interface Database {
     }
     Enums: {
       account_type_enum: "personal" | "shared"
+      approval_status_enum: "pending" | "approved" | "rejected"
     }
     CompositeTypes: {
       [_ in never]: never
