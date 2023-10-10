4fea61a062771080777479b9fdf6be096c805d88
Julius Niiniranta <juliusn@users.noreply.github.com>
2023-10-10 22:06:14 +0300

    clean up middleware

--- middleware.ts
+++ middleware.ts
@@ -8,17 +8,12 @@ const intlMiddleware = createMiddleware({
 });
 
 export default async function middleware(req: NextRequest) {
-  try {
-    const defaultLocale = req.headers.get('x-default-locale') || 'en';
-    const res = intlMiddleware(req);
-    res.headers.set('x-default-locale', defaultLocale);
-    const supabase = createMiddlewareClient<Database>({ req, res });
-    await supabase.auth.getSession();
-    return res;
-  } catch (error) {
-    console.error(`MIDDLEWARE ERROR`);
-    throw error;
-  }
+  const defaultLocale = req.headers.get('x-default-locale') || 'en';
+  const res = intlMiddleware(req);
+  res.headers.set('x-default-locale', defaultLocale);
+  const supabase = createMiddlewareClient<Database>({ req, res });
+  await supabase.auth.getSession();
+  return res;
 }
 
 export const config = {
