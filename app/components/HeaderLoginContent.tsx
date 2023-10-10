9dce46f61d25a4fcce90b0f050a8516cd2da9ef8
Julius Niiniranta <juliusn@users.noreply.github.com>
2023-10-11 00:10:11 +0300

    use tabs for header login navigation

--- app/components/HeaderLoginContent.tsx
+++ app/components/HeaderLoginContent.tsx
@@ -1,7 +1,12 @@
 'use client';
 
-import { ActionIcon, Button, Group, Menu } from '@mantine/core';
-import { IconKey, IconLogin2, IconUserPlus } from '@tabler/icons-react';
+import { ActionIcon, Group, Menu, Tabs, UnstyledButton } from '@mantine/core';
+import {
+  IconChevronDown,
+  IconKey,
+  IconLogin2,
+  IconUserPlus,
+} from '@tabler/icons-react';
 import { usePathname, useRouter } from 'next-intl/client';
 
 export function HeaderLoginContent({
@@ -31,9 +36,12 @@ export function HeaderLoginContent({
         <Group hiddenFrom="xs">
           <Menu position="bottom-end">
             <Menu.Target>
-              <ActionIcon variant="outline">
-                <IconKey />
-              </ActionIcon>
+              <UnstyledButton variant="transparent">
+                <Group gap={7}>
+                  <IconKey size={20} stroke={1} />
+                  <IconChevronDown size={20} stroke={1} />
+                </Group>
+              </UnstyledButton>
             </Menu.Target>
             <Menu.Dropdown>
               {items.map(({ path, icon, label }, i) => {
@@ -50,33 +58,20 @@ export function HeaderLoginContent({
             </Menu.Dropdown>
           </Menu>
         </Group>
-        <Group gap={8} visibleFrom="xs" hiddenFrom="md">
-          {items.map(({ path, label }, i) => {
-            return (
-              <Button
-                key={i}
-                onClick={() => router.push(path)}
-                disabled={pathname === path}
-                size="xs"
-                variant="outline">
-                {label}
-              </Button>
-            );
-          })}
-        </Group>
-        <Group visibleFrom="md">
-          {items.map(({ path, label }, i) => {
-            return (
-              <Button
-                key={i}
-                onClick={() => router.push(path)}
-                disabled={pathname === path}
-                variant="outline">
-                {label}
-              </Button>
-            );
-          })}
-        </Group>
+        <Tabs
+          value={pathname}
+          onChange={(value) => value && router.push(value)}
+          visibleFrom="xs">
+          <Tabs.List>
+            {items.map(({ path, label }, i) => {
+              return (
+                <Tabs.Tab key={i} value={path}>
+                  {label}
+                </Tabs.Tab>
+              );
+            })}
+          </Tabs.List>
+        </Tabs>
       </Group>
     </>
   );
