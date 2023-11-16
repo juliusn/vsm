d667d83ede577d4081d0ce10c8e494bce0a5b901
Julius Niiniranta <juliusn@users.noreply.github.com>
2023-11-16 14:56:00 +0200

    run prettier

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -4,61 +4,61 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       profiles: {
         Row: {
-          account_type: Database["public"]["Enums"]["account_type_enum"]
-          approval_status: Database["public"]["Enums"]["approval_status_enum"]
-          approval_status_set_by: string | null
-          id: string
-          updated_at: string | null
-          user_name: string
-        }
+          account_type: Database['public']['Enums']['account_type_enum'];
+          approval_status: Database['public']['Enums']['approval_status_enum'];
+          approval_status_set_by: string | null;
+          id: string;
+          updated_at: string | null;
+          user_name: string;
+        };
         Insert: {
-          account_type?: Database["public"]["Enums"]["account_type_enum"]
-          approval_status?: Database["public"]["Enums"]["approval_status_enum"]
-          approval_status_set_by?: string | null
-          id: string
-          updated_at?: string | null
-          user_name: string
-        }
+          account_type?: Database['public']['Enums']['account_type_enum'];
+          approval_status?: Database['public']['Enums']['approval_status_enum'];
+          approval_status_set_by?: string | null;
+          id: string;
+          updated_at?: string | null;
+          user_name: string;
+        };
         Update: {
-          account_type?: Database["public"]["Enums"]["account_type_enum"]
-          approval_status?: Database["public"]["Enums"]["approval_status_enum"]
-          approval_status_set_by?: string | null
-          id?: string
-          updated_at?: string | null
-          user_name?: string
-        }
+          account_type?: Database['public']['Enums']['account_type_enum'];
+          approval_status?: Database['public']['Enums']['approval_status_enum'];
+          approval_status_set_by?: string | null;
+          id?: string;
+          updated_at?: string | null;
+          user_name?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "profiles_id_fkey"
-            columns: ["id"]
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-    }
+            foreignKeyName: 'profiles_id_fkey';
+            columns: ['id'];
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       install_available_extensions_and_test: {
-        Args: Record<PropertyKey, never>
-        Returns: boolean
-      }
-    }
+        Args: Record<PropertyKey, never>;
+        Returns: boolean;
+      };
+    };
     Enums: {
-      account_type_enum: "personal" | "shared"
-      approval_status_enum: "pending" | "approved" | "rejected"
-    }
+      account_type_enum: 'personal' | 'shared';
+      approval_status_enum: 'pending' | 'approved' | 'rejected';
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
