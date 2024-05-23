ccc89a4eda89ed344bd60640b5546f82a79b145a
Julius Niiniranta <juliusn@users.noreply.github.com>
2024-05-23 14:52:25 +0300

    orders page

---
+++ app/[locale]/orders/NewOrderForm.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export function NewOrderForm() {
+  return <div>NewOrderForm</div>;
+}
--- app/[locale]/orders/page.tsx
+++ app/[locale]/orders/page.tsx
@@ -1,3 +1,19 @@
+import { Container, Group, Stack, Title } from '@mantine/core';
+import { useTranslations } from 'next-intl';
+import { NewOrder } from './NewOrder';
+
 export default function OrdersPage() {
-  return <div>OrdersPage</div>;
+  const t = useTranslations('Orders');
+  return (
+    <>
+      <Container>
+        <Stack>
+          <Group justify="space-between">
+            <Title size="h4">{t('title')}</Title>
+            <NewOrder />
+          </Group>
+        </Stack>
+      </Container>
+    </>
+  );
 }
