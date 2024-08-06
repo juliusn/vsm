02acc7edb9f762d3ae6709932896cfbed8199824
Julius Niiniranta <juliusn@users.noreply.github.com>
2024-08-06 13:49:54 +0300

    update eslint config with create-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,37 @@
-import { fixupConfigRules } from "@eslint/compat";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [...fixupConfigRules(compat.extends(
-    "next/core-web-vitals",
-    "prettier",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-)), {
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
     rules: {
-        "react/react-in-jsx-scope": "off",
-        "no-console": "error",
-
-        "no-restricted-imports": ["error", {
-            paths: [{
-                name: "next/link",
-                importNames: ["default"],
-                message: "Please use { Link } from \"next-intl/navigation\" instead.",
-            }, {
-                name: "next/navigation",
-                importNames: ["redirect", "usePathname", "useRouter"],
-                message: "Please use { redirect, usePathname, useRouter } from \"next-intl/navigation\" instead.",
-            }],
-        }],
+      'react/react-in-jsx-scope': 'off',
+      'no-console': 'error',
+      'no-restricted-imports': [
+        'error',
+        {
+          paths: [
+            {
+              name: 'next/link',
+              importNames: ['default'],
+              message:
+                'Please use { Link } from "next-intl/navigation" instead.',
+            },
+            {
+              name: 'next/navigation',
+              importNames: ['redirect', 'usePathname', 'useRouter'],
+              message:
+                'Please use { redirect, usePathname, useRouter } from "next-intl/navigation" instead.',
+            },
+          ],
+        },
+      ],
     },
-}];
+  },
+];
