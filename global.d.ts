05e0ca80591b0672dce20074fa05d687d5a864aa
Julius Niiniranta <juliusn@users.noreply.github.com>
2024-09-03 14:41:19 +0300

    fix types path

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,8 @@
-import type { Database as DB } from '@/lib/database.types';
+import type { Database as DB } from '@/lib/types/database.types';
 
 declare global {
   type Database = DB;
   type Profile = Database['public']['Tables']['profiles']['Row'];
 }
 
-type Messages = typeof import('./messages/en.json');
-declare interface IntlMessages extends Messages {}
+type IntlMessages = typeof import('./messages/en.json');
