5622c85619281667a3e3c1c7796437ed3402ef0f
Julius Niiniranta <juliusn@users.noreply.github.com>
2024-12-02 15:59:54 +0200

    Fetch data for the new order form on the client.

--- app/[locale]/orders/layout.tsx
+++ app/[locale]/orders/layout.tsx
@@ -1,7 +1,4 @@
-import { DataUnavailableAlert } from '@/app/components/DataUnavailableAlert';
-import { createClient } from '@/lib/supabase/server';
-import { Group, Stack, Title } from '@mantine/core';
-import { getTranslations } from 'next-intl/server';
+import { Stack } from '@mantine/core';
 import { NewOrderContent } from './NewOrderContent';
 
 export default async function OrdersLayout({
@@ -9,52 +6,9 @@ export default async function OrdersLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const t = await getTranslations('OrdersLayout');
-  const supabase = createClient();
-
-  const [locationsResponse, portAreasResponse, berthsResponse] =
-    await Promise.all([
-      supabase
-        .from('locations')
-        .select()
-        .eq('enabled', true)
-        .order('location_name'),
-      supabase
-        .from('port_areas')
-        .select()
-        .eq('enabled', true)
-        .order('port_area_name'),
-      supabase.from('berths').select().eq('enabled', true).order('berth_name'),
-    ]);
-
-  if (
-    locationsResponse.error ||
-    portAreasResponse.error ||
-    berthsResponse.error
-  ) {
-    return <DataUnavailableAlert />;
-  }
-
-  const locations = locationsResponse.data;
-  const locodes = locations.map((location) => location.locode);
-  const portAreas = portAreasResponse.data.filter((portArea) =>
-    locodes.includes(portArea.locode)
-  );
-  const portAreaCodes = portAreas.map((portArea) => portArea.port_area_code);
-  const berths = berthsResponse.data.filter((berth) =>
-    portAreaCodes.includes(berth.port_area_code)
-  );
-
   return (
     <Stack>
-      <Group justify="space-between">
-        <Title size="h4">{t('title')}</Title>
-        <NewOrderContent
-          locations={locations}
-          portAreas={portAreas}
-          berths={berths}
-        />
-      </Group>
+      <NewOrderContent />
       {children}
     </Stack>
   );
