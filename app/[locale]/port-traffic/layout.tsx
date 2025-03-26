c51bb228837e923e6d6889b275661cf7eaa00d55
Julius Niiniranta <juliusn@users.noreply.github.com>
2025-03-26 15:06:46 +0200

    Sort port data
    Sort locations, portAreas and berths in the order they should be displayed in the select inputs.

--- app/[locale]/port-traffic/layout.tsx
+++ app/[locale]/port-traffic/layout.tsx
@@ -49,8 +49,7 @@ export default async function PortTrafficLayout({
         .filter(
           (vessel, index, array) =>
             array.findIndex((v) => v.imo === vessel.imo) === index
-        )
-        .sort((a, b) => a.timestamp - b.timestamp);
+        );
       const locations = locationsResponse.data;
       const locodes = locations.map((location) => location.locode);
       const filteredPortAreas = portAreasResponse.data.filter((portArea) =>
@@ -59,9 +58,38 @@ export default async function PortTrafficLayout({
       const filteredPortAreaCodes = filteredPortAreas.map(
         (portArea) => portArea.port_area_code
       );
-      const filteredBerths = berthsResponse.data.filter((berth) =>
-        filteredPortAreaCodes.includes(berth.port_area_code)
+      const filteredBerths = berthsResponse.data.filter(
+        (berth) =>
+          locodes.includes(berth.locode) &&
+          filteredPortAreaCodes.includes(berth.port_area_code)
       );
+      vessels.sort((a, b) => a.timestamp - b.timestamp);
+      locations.sort((a, b) => a.location_name.localeCompare(b.location_name));
+      filteredPortAreas.sort((a, b) => {
+        const locIndexA = locations.findIndex((loc) => loc.locode === a.locode);
+        const locIndexB = locations.findIndex((loc) => loc.locode === b.locode);
+        if (locIndexA !== locIndexB) {
+          return locIndexA - locIndexB;
+        }
+        return a.port_area_name.localeCompare(b.port_area_name);
+      });
+      filteredBerths.sort((a, b) => {
+        const locIndexA = locations.findIndex((loc) => loc.locode === a.locode);
+        const locIndexB = locations.findIndex((loc) => loc.locode === b.locode);
+        if (locIndexA !== locIndexB) {
+          return locIndexA - locIndexB;
+        }
+        const portAreaIndexA = filteredPortAreas.findIndex(
+          (pa) => pa.port_area_code === a.port_area_code
+        );
+        const portAreaIndexB = filteredPortAreas.findIndex(
+          (pa) => pa.port_area_code === b.port_area_code
+        );
+        if (portAreaIndexA !== portAreaIndexB) {
+          return portAreaIndexA - portAreaIndexB;
+        }
+        return a.berth_name.localeCompare(b.berth_name);
+      });
       return {
         vessels,
         locations,
