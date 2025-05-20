72a84ba6a369e324b6645ab20f0879b6cba46d31
Julius Niiniranta <juliusn@users.noreply.github.com>
2025-05-20 14:17:27 +0300

    Use shared getLocationInputItems

--- app/[locale]/port-traffic/LocationInputs.tsx
+++ app/[locale]/port-traffic/LocationInputs.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { ComboboxItem, ComboboxItemGroup } from '@mantine/core';
+import { getLocationInputItems } from '@/lib/getLocationInputItems';
+import { DockingFormValues } from '@/lib/types/docking';
 import { UseFormReturnType } from '@mantine/form';
 import { useMemo } from 'react';
 import { BerthInput } from './BerthInput';
 import { LocodeInput } from './LocodeInput';
 import { PortAreaInput } from './PortAreaInput';
 import { usePortData } from './PortDataContext';
-import { DockingFormValues, PortAreaIdentifier } from '@/lib/types/docking';
 
 export function LocationInputs({
   locode,
@@ -23,7 +23,7 @@ export function LocationInputs({
 }) {
   const { locations, portAreas, berths } = usePortData();
   const { portAreaItems, berthsItems } = useMemo(
-    () => getInputItems(locations, portAreas, berths, locode, portArea),
+    () => getLocationInputItems(locations, portAreas, berths, locode, portArea),
     [locations, portAreas, berths, locode, portArea]
   );
 
@@ -46,75 +46,4 @@ export function LocationInputs({
       />
     </>
   );
-}
-
-function getInputItems(
-  locations: AppTypes.Location[],
-  portAreas: AppTypes.PortArea[],
-  berths: AppTypes.Berth[],
-  locode: string,
-  portArea: string
-) {
-  const filteredPortAreas = portAreas.filter((portArea) =>
-    locode ? portArea.locode === locode : true
-  );
-
-  const portAreaItems = locations.map(
-    ({ locode, location_name }): ComboboxItemGroup => ({
-      group: location_name,
-      items: filteredPortAreas
-        .filter((portArea) => portArea.locode === locode)
-        .map(
-          ({ locode, port_area_code, port_area_name }): ComboboxItem => ({
-            value: JSON.stringify({ locode, port_area_code }),
-            label: `${port_area_code} - ${port_area_name}`,
-          })
-        ),
-    })
-  );
-
-  const portAreaId = portArea
-    ? (JSON.parse(portArea) as PortAreaIdentifier)
-    : null;
-
-  const filteredPortAreaLocationNames = filteredPortAreas.map(
-    ({ locode }) =>
-      locations.find((location) => location.locode === locode)?.location_name
-  );
-
-  const filteredBerths = berths.filter(
-    (berth) =>
-      (locode ? berth.locode === locode : true) &&
-      (portAreaId
-        ? portAreaId.locode === berth.locode &&
-          portAreaId.port_area_code === berth.port_area_code
-        : true)
-  );
-
-  const berthsItems = filteredPortAreas.map(
-    ({ locode, port_area_code, port_area_name }, index): ComboboxItemGroup => ({
-      group: `${filteredPortAreaLocationNames[index]} ${port_area_name}`,
-      items: filteredBerths
-        .filter(
-          (berth) =>
-            berth.locode === locode && berth.port_area_code === port_area_code
-        )
-        .map(
-          ({
-            locode,
-            port_area_code,
-            berth_code,
-            berth_name,
-          }): ComboboxItem => ({
-            value: JSON.stringify({ locode, port_area_code, berth_code }),
-            label:
-              berth_code === berth_name
-                ? berth_code
-                : `${berth_code} - ${berth_name}`,
-          })
-        ),
-    })
-  );
-
-  return { portAreaItems, berthsItems };
 }
